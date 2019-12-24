c4c1cf1425d8cf79695e00a6753f366154d84cae
dylan <dylan@psilly.com>
2019-12-24 12:05:05 -0500

    plot

--- golden.py
+++ golden.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-from scipy.constants import golden as ϕ
 from random import random
 
-def golden_point_generator(seed, N):
+#from scipy.constants import golden
+#iphi = 1/golden
+iphi = (1 - 5 ** .5) * -.5
+
+def golden_point_generator(N):
   points = [x[:] for x in [[None] * 2] * N]
 
   # set the initial first coodinate
@@ -15,20 +18,19 @@ def golden_point_generator(seed, N):
     points[i][1] = x
 
     # keep the minimum
-    # @todo seems this can't happen since python doens't overflow ints
     if x < minx:
       minx = x
       idx = i
 
     # increment the coordinate
-    x += ϕ
+    x += iphi
 
     if x >= 1:
       x -= 1
 
   # find the first Fibonacci >= N
   f, fp, parity = 1, 1, 0
-  while (f + fp < N):
+  while f + fp < N:
     f, fp = fp, f+fp
     parity += 1
 
@@ -37,16 +39,49 @@ def golden_point_generator(seed, N):
   if parity & 1:
     inc, dec = f, fp
 
-  # permute the first coordinate
+  # permute the first coordinates
+  points[0][0] = points[idx][1]
+  for i in range(1, N):
+    if idx < dec:
+      idx += inc
+      if idx >= dec:
+        idx -= dec
+    else:
+      idx -= dec
+    points[i][0] = points[idx][1]
 
   # set the second initial coordinate
+  y = random()
 
-  print(inc, dec)
+  # set the second coordinates
+  for i in range(N):
+    points[i][1] = y
 
-  num = 0
-  while num < 10:
-    yield num
-    num += ϕ
+    # increment the coordinate
+    y += iphi
 
-for i in golden_point_generator(seed=0, N=16):
-  print(i)
+    if y >= 1:
+      y -= 1
+
+  return points
+
+if __name__ == '__main__':
+  import sys
+  from pprint import pprint
+
+  N = int(sys.argv[1]) if len(sys.argv) > 1 else 6
+
+  points = golden_point_generator(N)
+
+  pprint(points)
+
+
+  import matplotlib.pyplot as plt
+  plt.scatter(*zip(*points))
+  plt.show()
+  #import numpy as np
+  #import matplotlib.pyplot as plt
+  #plt.imshow(points)
+  #plt.colorbar()
+  #plt.show()
+
